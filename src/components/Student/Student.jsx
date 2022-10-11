6f5bd1ada3c6acf40aadcfa8d4a50fa3a17dd4e8
lilyshowlader <lilyshowlader@gmail.com>
2022-10-11 18:56:06 -0400

    set up app and student component

---
+++ src/components/Student/Student.jsx
@@ -0,0 +1,14 @@
+
+const Student = (props) => {
+  return (  
+    <>
+    <h2>{props.student.name}</h2>
+    <h4>{props.student.bio}</h4>
+    </>
+  );
+}
+ 
+export default Student;
+
+// I want the students name and bio displayed
+// I want the score displayed
